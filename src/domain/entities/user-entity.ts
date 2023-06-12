d8b0039f676e3a6e1705286004e12dc69913a2b7
Daniel Ruiz <danielruimar@gmail.com>
2023-06-12 11:11:58 +0200

    Added first lybraries

---
+++ src/domain/entities/user-entity.ts
@@ -0,0 +1,83 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - email
+ *        - password
+ *        - name
+ *      properties:
+ *        email:
+ *          type: string
+ *          format: email
+ *          description: Email del autor
+ *        password:
+ *          type: string
+ *          minLength: 8
+ *          description: Contraseña del autor
+ *        name:
+ *          type: string
+ *          minLength: 3
+ *          maxLength: 22
+ *          description: Nombre del autor
+ */
+
+import mongoose from "mongoose";
+
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+const Schema = mongoose.Schema;
+
+export interface IUser {
+  email: string;
+  password: string;
+  name: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true, // indica que no puede haber otra entidad con esta propiedad que tenga el mismo valor.
+      validate: {
+        validator: (text: string) => validator.isEmail, // Validamos haciendo uso de la librería validator y la función isEmail que incorpora.
+        message: "Email incorrecto"
+      },
+      required: true
+    },
+    password: {
+      type: String,
+      trim: true,
+      unique: true,
+      minLength: [8, "La contraseña debe tener al menos 8 caracteres"],
+      select: false, // Indica que no lo deseamos mostrar cuando se realicen las peticiones.
+      required: true
+    },
+    name: { type: String, trim: true, minLength: [3, "Al menos tres letras para el nombre"], maxLength: [22, "Nombre demasiado largo, máximo de 22 caracteres"], required: true },
+
+  },
+  { timestamps: true } // Cada vez que se modifique un documento refleja la hora y fecha de modificación
+);
+
+// Cada vez que se guarde un usuario encriptamos la contraseña
+userSchema.pre("save", async function (next) {
+  try {
+    // Si la password estaba encriptada, no la encriptaremos de nuevo.
+    if (this.isModified("password")) {
+      // Si el campo password se ha modificado
+      const saltRounds = 10;
+      const passwordEncrypted = await bcrypt.hash(this.password, saltRounds); // Encriptamos la contraseña
+      this.password = passwordEncrypted; // guardamos la password en la entidad User
+      next();
+    }
+  } catch (error) {
+    next();
+  }
+});
+
+// Creamos un modelo para que siempre que creamos un user valide contra el Schema que hemos creado para ver si es valido.
+export const User = mongoose.model<IUser>("User", userSchema);
---
+++ src/domain/services/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { User } from "../entities/user-entity"
+import { verifyToken } from "../../utils/token";
+
+import {
+  type Response,
+  type NextFunction,
+} from "express";
+
+export const isAuth = async (req: any, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new Error("No tienes autorización para realizar esta operación");
+    }
+
+    const decodedInfo = verifyToken(token);
+    const user = await User.findOne({ email: decodedInfo.email }).select("+password");
+    if (!user) {
+      throw new Error("No tienes autorización para realizar esta operación");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "No tienes autorización para realizar esta operación" });
+  }
+};
