89000c16d4a17b19d72473406a82d6400b5ff88e
Daniel Ruiz <danielruimar@gmail.com>
2023-06-13 13:13:51 +0200

    Fix problems

--- src/utils/users-cakes-categories-orders.seed.ts
+++ src/utils/users-cakes-categories-orders.seed.ts
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 import { mongoConnect } from "../domain/repositories/mongo-repository"; // Importamos el archivo de conexión a la BBDD
 
 import { resetUsers } from "../domain/utils/resetUsers";
-import { resetCakes } from "./resetCakes";
-import { resetCategories } from "./resetCategories";
+// import { resetCakes } from "./resetCakes";
+// import { resetCategories } from "./resetCategories";
 import { tartinchisRelations } from "./tartinchisRelations";
 
 const seedFunction = async (): Promise<void> => {
   try {
     await mongoConnect();
     await resetUsers();
-    await resetCakes()
-    await resetCategories()
+    // await resetCakes()
+    // await resetCategories()
     await tartinchisRelations()
   } catch (error) {
     console.error(error);
